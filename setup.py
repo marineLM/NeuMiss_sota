c3fced07b20a59688b2eac43bba5e20cd24eeb7a
Alexandre Pérez <alexperez@wanadoo.fr>
2021-09-24 13:22:37 +0200

    Change setup file to setup.py

---
+++ setup.py
@@ -0,0 +1,51 @@
+import setuptools
+# from setuptools import setup
+
+# setup(
+#     name='neumiss',
+#     version='0.0.1',
+#     author='Marine Le Morvan and Alexandre Perez-Lebel',
+#     author_email='alexandre.perez@inria.fr',
+#     packages=['neumiss', 'neumiss.test'],
+#     scripts=['bin/script1', 'bin/script2'],
+#     url='http://pypi.python.org/pypi/PackageName/',
+#     license='LICENSE.txt',
+#     description='An awesome package that does something',
+#     long_description=open('README.txt').read(),
+#     install_requires=[
+#         "Django >= 1.1.1",
+#         "pytest",
+#     ],
+# )
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="neumiss",
+    version="0.0.1",
+    author="Marine Le Morvan and Alexandre Perez-Lebel",
+    author_email="alexandre.perez@inria.fr",
+    description="Implement the NeuMiss network",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/marineLM/NeuMiss_sota",
+    project_urls={
+        "Bug Tracker": "https://github.com/marineLM/NeuMiss_sota/issues",
+    },
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    package_dir={"": "."},
+    packages=setuptools.find_packages(where="."),
+    python_requires=">=3.6",
+    install_requires=[
+        'numpy',
+        'scipy',
+        'sklearn',
+        'torch',
+        'pytorch-lightning',
+    ],
+)
