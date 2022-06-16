e529bd284ca67b7eabb9f23f1c2f5e9a69aa5607
Alexandre Perez-Lebel <alexperez@wanadoo.fr>
2022-06-16 11:35:16 +0200

    Update NeuMissBlock

--- neumiss/NeuMissBlock.py
+++ neumiss/NeuMissBlock.py
@@ -21,9 +21,9 @@ class NeuMissBlockBase(nn.Module):
     def __init__(self, n_features, depth):
         super().__init__()
         self.depth = depth
-        self.W = nn.Parameter(torch.empty(n_features, n_features, dtype=torch.double))
-        # self.Wc = nn.Parameter(torch.empty(n_features, n_features, dtype=torch.double))
-        self.mu = nn.Parameter(torch.empty(n_features, dtype=torch.double))
+        self.W = nn.Parameter(torch.empty(n_features, n_features, dtype=torch.float))
+        # self.Wc = nn.Parameter(torch.empty(n_features, n_features, dtype=torch.float))
+        self.mu = nn.Parameter(torch.empty(n_features, dtype=torch.float))
 
     def forward(self, x):
         m = torch.isnan(x)
@@ -46,14 +46,14 @@ class NeuMissBlock(nn.Module):
     def __init__(self, n_features, depth):
         super().__init__()
         self.depth = depth
-        # self.W = nn.Parameter(torch.empty(n_features, n_features, dtype=torch.double))
-        # self.Wc = nn.Parameter(torch.empty(n_features, n_features, dtype=torch.double))
-        self.mu = nn.Parameter(torch.empty(n_features, dtype=torch.double))
+        # self.W = nn.Parameter(torch.empty(n_features, n_features, dtype=torch.float))
+        # self.Wc = nn.Parameter(torch.empty(n_features, n_features, dtype=torch.float))
+        self.mu = nn.Parameter(torch.empty(n_features, dtype=torch.float))
 
         self.linear = nn.Linear(n_features, n_features, bias=False)
 
     def forward(self, x):
-        # x = x.double()
+        x = x
         m = torch.isnan(x)
         x = torch.nan_to_num(x)
         h = x - ~m*self.mu
@@ -62,16 +62,18 @@ class NeuMissBlock(nn.Module):
         # nn.Linear(n_features, n_features, bias=False)
 
         for _ in range(self.depth):
-            print(h.float())
-            print(self.linear.weight)
-            h = self.linear(h.float())*~m
+            # print(h.float())
+            # print(self.linear.weight)
+            h = self.linear(h)*~m
             # h = torch.matmul(h, self.W)*~m
             h += h_res
+            # h = h.double()
 
         return h
 
-    def reset_parameters(self) -> None:
-        nn.init.kaiming_uniform_(self.linear.weight, a=math.sqrt(5))
+    # def reset_parameters(self) -> None:
+    #     nn.init.kaiming_uniform_(self.linear.weight, a=math.sqrt(5))
 
 # nn.Linear
+
 nn.ReLU
--- tests/test_neumiss.py
+++ tests/test_neumiss.py
@@ -3,29 +3,61 @@ from torch import nn
 import math 
 import pytest
 from neumiss.NeuMissBlock import NeuMissBlock, NeuMissBlockBase
+import numpy as np
 
-
-@pytest.mark.parametrize('n_features', [10])
-@pytest.mark.parametrize('depth', [1])
+@pytest.mark.parametrize('n_features', [2, 10])
+@pytest.mark.parametrize('depth', [1, 3])
 def test_neumissblock(n_features, depth):
     # m = NeuMissBlock(n_features, depth)
 
-    W = nn.Parameter(torch.empty(n_features, n_features, dtype=torch.float))
+
+    rng = np.random.RandomState(0)
+    _W = rng.uniform(size=(n_features, n_features))
+    # W = nn.Parameter(torch.empty(n_features, n_features, dtype=torch.float))
+    # print(W)
+    # return
 
     torch.manual_seed(0)
     # nn.init.kaiming_uniform_(W, a=math.sqrt(5))
-    x = torch.rand(n_features)
+    x = np.array(torch.rand(n_features))
+
+    mask = rng.binomial(1, 0.5, size=(n_features))
+    np.putmask(x, mask, np.nan)
+    print(mask)
+    print(x)
+    x = torch.Tensor(x)
 
     # print(W)
     # print(x)
 
     # for net in [NeuMissBlock, NeuMissBlockBase]:
     m = NeuMissBlock(n_features, depth)
-    # m.linear.weight = W
-    torch.nn.init.xavier_uniform(m.linear.weight)
+    # linear = nn.Linear(n_features, n_features, bias=False)
+    with torch.no_grad():
+        W = nn.Parameter(torch.tensor(_W, dtype=torch.float))
+        # linear.weight = W
+        m.linear.weight = W
+        m.mu = nn.Parameter(torch.zeros_like(m.mu))
+    # torch.nn.init.xavier_uniform(m.linear.weight)
     # m.W = nn.Parameter(torch.zeros_like(m.W))
     # m.mu = nn.Parameter(torch.zeros_like(m.mu))
     # m.reset_parameters()
     # print(x)
+    # y1 = linear(x)
     y1 = m.forward(x)
     print(y1)
+
+
+    m2 = NeuMissBlockBase(n_features, depth)
+    m2.W = nn.Parameter(torch.tensor(_W.T, dtype=torch.float))
+    # m2.W = nn.Parameter(torch.zeros_like(m2.W, dtype=torch.float))
+    m2.mu = nn.Parameter(torch.zeros_like(m.mu, dtype=torch.float))
+    y2 = m2.forward(x)
+    print(y2)
+
+
+    assert torch.allclose(y1.double(), y2.double())
+    assert torch.allclose(m2.W.T.double(), m.linear.weight.double())
+
+    # a1 = torch.tensor(np.pi, dtype=torch.float)
+    # a2 = torch.tensor(np.pi, dtype=torch.double)
