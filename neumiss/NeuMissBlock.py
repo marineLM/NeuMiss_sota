accf5be997bf43423b613a70b954ebe3c0fbf556
Alexandre Perez-Lebel <alexperez@wanadoo.fr>
2022-06-17 13:35:01 +0200

    Update block

--- neumiss/NeuMissBlock.py
+++ neumiss/NeuMissBlock.py
@@ -1,7 +1,7 @@
 import torch
 from torch import nn
 from torch import Tensor
-import math
+from torch.types import _dtype
 
 
 class Mask(nn.Module):
@@ -17,7 +17,7 @@ class Mask(nn.Module):
 
 
 class SkipConnection(nn.Module):
-    __constants__ = ['mask']
+    __constants__ = ['value']
     value: Tensor
 
     def __init__(self, value: Tensor):
@@ -28,42 +28,33 @@ class SkipConnection(nn.Module):
         return input + self.value
 
 
-class NeuMissBlockBase(nn.Module):
-
-    def __init__(self, n_features, depth):
-        super().__init__()
-        self.depth = depth
-        self.W = nn.Parameter(torch.empty(n_features, n_features, dtype=torch.float))
-        # self.Wc = nn.Parameter(torch.empty(n_features, n_features, dtype=torch.float))
-        self.mu = nn.Parameter(torch.empty(n_features, dtype=torch.float))
-
-    def forward(self, x):
-        m = torch.isnan(x)
-        x = torch.nan_to_num(x)
-        h = x - ~m*self.mu
-        h_res = x - ~m*self.mu
-
-        for _ in range(self.depth):
-            h = torch.matmul(h, self.W)*~m
-            h += h_res
-
-        return h
-
-    def reset_parameters(self) -> None:
-        nn.init.kaiming_uniform_(self.W, a=math.sqrt(5))
-
-
 class NeuMissBlock(nn.Module):
+    """Implement the NeuMiss block from "What’s a good imputation to predict
+    with missing values?" by Marine Le Morvan, Julie Josse, Erwan Scornet,
+    Gael Varoquaux."""
 
-    def __init__(self, n_features, depth, dtype=torch.float):
+    def __init__(self, n_features: int, depth: int,
+                 dtype: _dtype = torch.float) -> None:
+        """
+        Parameters
+        ----------
+        n_features : int
+            Dimension of inputs and outputs of the NeuMiss block.
+        depth : int
+            Number of layers (Neumann iterations) in the NeuMiss block.
+        dtype : _dtype
+            Pytorch dtype for the parameters. Default: torch.float.
+
+        """
         super().__init__()
         self.depth = depth
         self.dtype = dtype
         self.mu = nn.Parameter(torch.empty(n_features, dtype=dtype))
         self.linear = nn.Linear(n_features, n_features, bias=False)
-        self.reset_parameters()
+        # self.linear.weight = nn.Parameter(self.linear.weight.type(dtype))
+        # self.reset_parameters()
 
-    def forward(self, x):
+    def forward(self, x: Tensor) -> Tensor:
         x = x.type(self.dtype)  # Cast tensor to appropriate dtype
         mask = Mask(x)  # Initialize mask non-linearity
         x = torch.nan_to_num(x)  # Fill missing values with 0
@@ -76,8 +67,19 @@ class NeuMissBlock(nn.Module):
         return layers(h)
 
     def reset_parameters(self) -> None:
+        torch.manual_seed(0)
         nn.init.normal_(self.mu)
-        nn.init.xavier_uniform_(self.linear.weight, gain=0.5)
+        # W = self.linear.weight
+        torch.manual_seed(0)
+        nn.init.normal_(self.linear.weight.float())
+        # nn.init.xavier_uniform_(self.linear.weight, gain=0.5)
+        print('Wb', self.linear.weight)
+        self.linear.weight = nn.Parameter(self.linear.weight.type(self.dtype))
+        print('Wa', self.linear.weight)
+
+        # print('Wdtyped', self.linear.weight.type(self.dtype))
+        # self.mu = nn.Parameter(self.mu.type(self.dtype))
+        # self.linear.weight = nn.Parameter(self.linear.weight.type(self.dtype))
 
     def extra_repr(self) -> str:
         return 'depth={}'.format(self.depth)
